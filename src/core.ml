ae4780c64edd94f896a8e0db351e1a4474f73bea
ZAN DoYe <zandoye@gmail.com>
2020-04-01 19:23:25 +0800

    export actions

--- src/core.ml
+++ src/core.ml
@@ -2,6 +2,8 @@ module Make (Concurrent:Mew.Concurrent.S) =
 struct
   module Base = Mew.Make(Modal)(Concurrent)
   module Interpret = Interpret.Make(Concurrent)
+  module Edit_action = Edit_action
+  module Vi_action = Vi_action
 
   class edit state =object
     inherit Base.edit state
